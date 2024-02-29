f707f30109378322623a6f25eedfc215c908ab1c
Courtney Grigsby <43795747+cgrigs@users.noreply.github.com>
2024-02-29 15:29:07 -0700

    Update submit_samples.sh

--- submit_samples.sh
+++ submit_samples.sh
@@ -7,10 +7,8 @@ FASTQ_DIR="/scratch/cgrigsb2/tet/TetMA/Tthermophila_fastqs"
 #SAMPLES=$(cut -f5 metadata.tsv | tail -n +2 | sort | uniq)
 
 for SAMPLE in $SAMPLES; do
-    # Construct a meaningful job name, e.g., based on the sample name
     JOB_NAME="process_${SAMPLE}"
     
-    # Submit the job
     sbatch --job-name=$JOB_NAME run_single_sample.sh $SAMPLE
 done
 '''
@@ -18,12 +16,8 @@ done
 
 # Iterate over FASTQ files
 for FASTQ_FILE in ${FASTQ_DIR}/*_R1_001.fastq.gz; do
-    # Extract the sample name/identifier from the FASTQ filename
     SAMPLE=$(basename ${FASTQ_FILE} "_R1_001.fastq.gz")
-    
-    # Construct a meaningful job name, e.g., based on the sample name
     JOB_NAME="process_${SAMPLE}"
     
-    # Submit the job
     sbatch --job-name=$JOB_NAME run_single_sample.sh $SAMPLE
 done
